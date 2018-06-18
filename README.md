9e598599520b2fa604268db7bc4ec955a62e3292
Anthony D. Blaom <anthony.blaom@gmail.com>
2018-06-19 10:18:49 +1200

    New repo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # KoalaKNN
+
+A K-nearest neighbor regressor for use in the
+[Koala](https://github.com/ablaom/Koala.jl) machine learning
+environment.
+
+## Model definition
+
+````
+KNNRegressor(K=1, metric=euclidean, kernel=reciprocal)
+````
+
+Returns a `K`-nearest neighbor model, with the specified `metric`. A
+weighted mean of target values is used in predictions, where the
+weight for a neighbor at distance `d` is `kernel(d)`. 
+
+See the documentation for `Koala` for the training API.
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,45 @@
+using Koala
+
 using KoalaKNN
 using Base.Test
+using DataFrames
 
-# write your own tests here
-@test 1 == 2
+X = [4 2 5 3;
+     2 1 6 0.0]
+
+@test KoalaKNN.distances_and_indices_of_closest(3, KoalaKNN.euclidean, X, [1, 1])[2] == [2, 4, 1]
+
+X = DataFrame(X')
+y = [2, 1, 3, 8]
+knn = KNNRegressor(K=3)
+tX = default_transformer_X(knn)
+tX.standardize = false
+knnM = Machine(knn, X, y, eachindex(y), transformer_X=tX)
+fit!(knnM, eachindex(y))
+
+r = 1 + 1/sqrt(5) + 1/sqrt(10)
+Xtest = DataFrame([1 1])
+yhat = (1 + 8/sqrt(5) + 2/sqrt(10))/r
+@test isapprox(predict(knnM, Xtest)[1], yhat)
+
+X, y = load_boston()
+knn = KNNRegressor()
+train, test = split(eachindex(y), 0.8)
+knnM = Machine(knn, X, y, train)
+fit!(knnM, train)
+
+u, v = @curve K 1:50 begin
+    knn.K = K
+    fit!(knnM)
+    err(knnM, test)
+end
+
+knn.K = u[indmin(v)]
+knn_errs = cv(knnM, eachindex(y))
+
+cnst = ConstantRegressor()
+cnstM = Machine(cnst, X, y, train)
+cnst_errs = cv(cnstM, eachindex(y))
+
+# check KNN is significantly better than constant prediction:
+@test compete(cnst_errs, knn_errs, alpha=0.01)[1] == '1'
