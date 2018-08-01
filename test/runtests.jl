7c83adfa37747dbd9a09dba8c87586a98dc2b970
Anthony D. Blaom <anthony.blaom@gmail.com>
2018-08-01 12:52:42 +1200

    Fix bug in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Koala
-
 using KoalaKNN
 using Base.Test
 using DataFrames
@@ -7,10 +6,11 @@ using DataFrames
 X = [4 2 5 3;
      2 1 6 0.0]
 
-@test KoalaKNN.distances_and_indices_of_closest(3, KoalaKNN.euclidean, X, [1, 1])[2] == [2, 4, 1]
+@test KoalaKNN.distances_and_indices_of_closest(3,
+        KoalaKNN.euclidean, X, [1, 1])[2] == [2, 4, 1]
 
 X = DataFrame(X')
-y = [2, 1, 3, 8]
+y = Float64[2, 1, 3, 8]
 knn = KNNRegressor(K=3)
 tX = default_transformer_X(knn)
 tX.standardize = false
@@ -18,13 +18,13 @@ knnM = Machine(knn, X, y, eachindex(y), transformer_X=tX)
 fit!(knnM, eachindex(y))
 
 r = 1 + 1/sqrt(5) + 1/sqrt(10)
-Xtest = DataFrame([1 1])
+Xtest = DataFrame([1.0 1.0])
 yhat = (1 + 8/sqrt(5) + 2/sqrt(10))/r
 @test isapprox(predict(knnM, Xtest)[1], yhat)
 
-X, y = load_boston()
+X, y = load_boston();
 knn = KNNRegressor()
-train, test = split(eachindex(y), 0.8)
+train, test = split(eachindex(y), 0.8);
 knnM = Machine(knn, X, y, train)
 fit!(knnM, train)
 
@@ -36,6 +36,8 @@ end
 
 knn.K = u[indmin(v)]
 knn_errs = cv(knnM, eachindex(y))
+
+println("error = ", mean(knn_errs), " ± ", std(knn_errs))
 
 cnst = ConstantRegressor()
 cnstM = Machine(cnst, X, y, train)
